7d80593e11fa2d168911712f7cd41d404810d736
Atul Mishra <147801384+atulmishra1997@users.noreply.github.com>
2024-05-18 18:53:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = 'http://localhost:4000'
+  const url = 'https://brandy-s-fast-food-backend-3.onrender.com'
 
   return (
     <div>
